7fa1f4c944888c8d165fc4e1c745fa8c5c057e70
anoint7483 <yabhishek8774@gmail.com>
2025-03-10 23:42:31 +0530

    working on this project again started

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-console.log("Welcome to music world");
-let songIndex = 0;
-//let audioElement = new Audio('songs/English songs/Feel my face.mp3');
-//let audioElement = new Audio('songs/English songs/Bam Bam.mp3');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar')
-let gif = document.getElementById('gif');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
+// console.log("Welcome to music world");
+// let songIndex = 0;
+// //let audioElement = new Audio('songs/English songs/Feel my face.mp3');
+// //let audioElement = new Audio('songs/English songs/Bam Bam.mp3');
+// let masterPlay = document.getElementById('masterPlay');
+// let myProgressBar = document.getElementById('myProgressBar')
+// let gif = document.getElementById('gif');
+// let songItems = Array.from(document.getElementsByClassName('songItem'));
 
-let songs = [
-    { songName: "2002", filepath: "songs/English songs/2002.mp3", coverpath: "" },
-    { songName: "Bam Bam", filepath: "songs/English songs/Bam Bam.mp3", coverpath: "" },
-    { songName: "Feel my face", filepath: "songs/English songs/Feel my face.mp3", coverpath: "" },
-    { songName: "", filepath: "", coverpath: "" },
-    { songName: "", filepath: "", coverpath: "" },
-    { songName: "", filepath: "", coverpath: "" },
-    { songName: "", filepath: "", coverpath: "" },
+// let songs = [
+//     { songName: "2002", filepath: "songs/English songs/2002.mp3", coverpath: "" },
+//     { songName: "Bam Bam", filepath: "songs/English songs/Bam Bam.mp3", coverpath: "" },
+//     { songName: "Feel my face", filepath: "songs/English songs/Feel my face.mp3", coverpath: "" },
+//     { songName: "", filepath: "", coverpath: "" },
+//     { songName: "", filepath: "", coverpath: "" },
+//     { songName: "", filepath: "", coverpath: "" },
+//     { songName: "", filepath: "", coverpath: "" },
     
-]
+// ]
 
-songItems.forEach((element, i) => {
-    console.log(element, i);
-    element.getElementsByTagName("img")[0].src = songs[i].coverpath;
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
-});
+// songItems.forEach((element, i) => {
+//     console.log(element, i);
+//     element.getElementsByTagName("img")[0].src = songs[i].coverpath;
+//     element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+// });
 
-masterPlay.addEventListener('click', () => {
-    if (audioElement.paused || audioElement.currentTime <= 0) {
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
-    }
-    else {
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
+// masterPlay.addEventListener('click', () => {
+//     if (audioElement.paused || audioElement.currentTime <= 0) {
+//         audioElement.play();
+//         masterPlay.classList.remove('fa-play-circle');
+//         masterPlay.classList.add('fa-pause-circle');
+//         gif.style.opacity = 1;
+//     }
+//     else {
+//         audioElement.pause();
+//         masterPlay.classList.remove('fa-pause-circle');
+//         masterPlay.classList.add('fa-play-circle');
+//         gif.style.opacity = 0;
 
-    }
-}
-)
-// Listen to events
-audioElement.addEventListener('timeupdate', () => {
-    // update seekbar
-    progress = parseInt(audioElement.currentTime / audioElement.duration * 100);
-    myProgressBar.value = progress;
-}
-)
-myProgressBar.addEventListener('change', () => {
-    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
-}
-)
+//     }
+// }
+// )
+// // Listen to events
+// audioElement.addEventListener('timeupdate', () => {
+//     // update seekbar
+//     progress = parseInt(audioElement.currentTime / audioElement.duration * 100);
+//     myProgressBar.value = progress;
+// }
+// )
+// myProgressBar.addEventListener('change', () => {
+//     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+// }
+// )
 
 
 
